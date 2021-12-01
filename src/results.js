7030169552e99c0e22e2f5bc1d12d9cb98971812
meekamae <meeka.cayabyab@gmail.com>
2021-12-01 15:32:37 -0800

    changed index

--- src/results.js
+++ src/results.js
@@ -4,7 +4,7 @@ import { Button, Form, Style, Render } from "react-bootstrap"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ReCAPTCHA from "react-google-recaptcha";
 import App from "./App"
-import Home from "./Index"
+import Home from "."
 
 export class Results extends Component {
 
